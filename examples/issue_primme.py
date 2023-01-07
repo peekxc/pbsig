FINALLy fixed bug related to overflow of eigenvalues. Needed a specialized _matmat for dimensions above 50!

examples/issue_primme.py
@@ -6,15 +6,21 @@ from scipy.sparse.linalg import LinearOperator, aslinearoperator, eigsh
 from scipy.sparse.csgraph import laplacian
 
 import primme
-m = 50
+m = 150
 T = csc_array(toeplitz([2, -1] + [0]*(m-3) + [-1]))
-ew_pm = np.sort(primme.eigsh(T, k=m, ncv=m, maxiter=1500, return_eigenvectors=False))
+
+ew_pm = np.sort(primme.eigsh(T, k=m, ncv=5, maxiter=10000, return_eigenvectors=False))
+
+TO = aslinearoperator(T)
+np.sort(primme.eigsh(TO, k=m, ncv=m, maxiter=10000, return_eigenvectors=False))
+
+
 
 from scipy.sparse import save_npz, load_npz
 L = load_npz("laplacian.npz")
 ew_pm = np.sort(primme.eigsh(L, k=L.shape[0], ncv=L.shape[0], maxiter=1500, return_eigenvectors=False))
-
-ew_pm = np.sort(primme.eigsh(L, k=L.shape[0], ncv=20, maxiter=5000, return_eigenvectors=False))
+LO = aslinearoperator(L)
+ew_pm = np.sort(primme.eigsh(LO, k=L.shape[0], ncv=20, maxiter=5000, return_eigenvectors=False))
 
 # from pbsig.linalg import parameterize_solver
 # solver = parameterize_solver(L, solver='jd')
@@ -42,12 +48,21 @@ ew_pm = np.sort(primme.eigsh(T, k=m, ncv=m, maxiter=1500, return_eigenvectors=Fa
 dataset = { k : pht_preprocess_pc(S, nd=64) for k, S in mpeg7(simplify=150).items() }
 X = dataset[('turtle',1)]
 S = cycle_graph(X)
-#L = up_laplacian(S, p=0, form='lo')
-L = up_laplacian(S, p=0, form='array')
+L = up_laplacian(S, p=0, form='lo')
+#L = up_laplacian(S, p=0, form='array')
 
-solver = parameterize_solver(L, solver='jd')
+solver = parameterize_solver(L, solver='jd', pp=0.01, maxiter=1500)
 solver(L)
 
+
+LO = up_laplacian(S, p=0, form='lo')
+LM = up_laplacian(S, p=0, form='array')
+
+import timeit
+timeit.timeit(lambda: LM @ x, number=100, setup=lambda: "x = np.random.uniform(size=LO.shape[0])")
+timeit.timeit(lambda: LO @ x, number=100, setup=lambda: "x = np.random.uniform(size=LO.shape[0])")
+LO.precompute()
+timeit.timeit(lambda: LO @ x, number=100, setup=lambda: "x = np.random.uniform(size=LO.shape[0])")
 
 save_npz("laplacian.npz", L)
 L = load_npz("laplacian.npz")

examples/mpeg7.py
@@ -2,6 +2,7 @@ import numpy as np
 from pbsig import * 
 from pbsig.linalg import * 
 from pbsig.datasets import mpeg7
+from pbsig.betti import *
 from pbsig.pht import pht_preprocess_pc, rotate_S1
 from pbsig.persistence import *
 from pbsig.simplicial import cycle_graph
@@ -12,46 +13,42 @@ from pbsig.vis import plot_complex
 # %% Load dataset 
 # NOTE: PHT preprocessing centers and scales each S to *approximately* the box [-1,1] x [-1,1]
 dataset = { k : pht_preprocess_pc(S, nd=64) for k, S in mpeg7(simplify=150).items() }
-
-# %% Generate a random set of rectangles in the upper half plane 
-def sample_rect_halfplane(n: int, area: tuple = (0, 0.05)):  
-  """ 
-  Generate random rectilinear boxes with area between [lb,ub] in upper-half plane via rejection sampling 
-  """
-  cc = 0
-  R = []
-  while cc < n:
-    r = np.sort(np.random.uniform(size=4, low = -1.0, high=1.0))
-    ra = (r[1]-r[0])*(r[3]-r[2])
-    if ra >= area[0] and ra <= area[1]:
-      R.append(r)
-    else: 
-      continue
-    cc += 1
-  return np.array(R)
-
 R = sample_rect_halfplane(1)[0,:]
 
 #%% Compute mu queries 
 X = dataset[('turtle',1)]
 S = cycle_graph(X)
-#L = up_laplacian(S, p=0, form='lo')
-L = up_laplacian(S, p=0, form='array')
+
+LM = up_laplacian(S, p=0, form='array')
+solver = parameterize_solver(LM, solver='jd')
+solver(LM)
+
+LO = up_laplacian(S, p=0, form='lo')
+LO.precompute()
+solver = parameterize_solver(LO, solver='lobpcg')
+solver(LO)
+
+M = np.random.uniform(size=LO.shape)
+np.allclose(np.ravel((LO @ M) - (LM @ M)), 0)
+
+LM @ M[:,0]
+LO @ M[:,0]
+
 # np.sort(primme.eigsh(T, k=m, ncv=m, maxiter=5000, return_eigenvectors=False))
 # np.sort(primme.eigsh(L, k=L.shape[0], ncv=L.shape[0], maxiter=5000, return_eigenvectors=False))
+L @ np.random.uniform(size=L.shape[0])
 
-solver = parameterize_solver(L, solver='jd')
-solver(L)
+parameterize_solver(L, solver='gd', ncv=20, maxiter=1500, return_unconverged=False)(L)
 
-parameterize_solver(L, solver='jd', ncv=L.shape[0], maxiter=10000)(L)
-numerical_rank(L)
+parameterize_solver(L, solver='jd')(L.precompute())
+
 
 fv = X @ np.array([0,1])
 K = MutableFiltration(S, f = lambda s: max(fv[s]))
 K = K.reindex_keys(np.arange(len(K)))
 
 ## Sample a couple rectangles in the upper half-plane and plot them 
-R = sample_rect_halfplane(1, area=(0.20, 1.00))
+R = sample_rect_halfplane(1, area=(0.10, 1.00))
 
 ## Plot all the diagrams, viridas coloring, as before
 from pbsig.color import bin_color

src/pbsig/linalg.py
@@ -209,6 +209,7 @@ def numerical_rank(A: Union[ArrayLike, spmatrix, LinearOperator], tol: float = N
     assert isinstance(tol, float), "Invalid tolerance"
     
     ## Use un-shifted operator 'A' + shifted solver to estimate the dimension of the nullspace w/ shift-invert mode
+    ## TODO: Preconditioning advice: https://caam37830.github.io/book/02_linear_algebra/sparse_linalg.html
     smallest_k = eigsh(A + tol*eye(A.shape[0]), k=k, which='LM', sigma=0.0, tol=tol, return_eigenvectors=False)
     dim_nullspace = sum(np.isclose(np.maximum(smallest_k - tol, 0.0), 0.0))
     
@@ -480,7 +481,9 @@ class UpLaplacian(LinearOperator):
     self._wfl = self._wfr = self._ws = UpLaplacian.identity_seq
     self.prepare()
     self._precompute_degree()
-    
+    self.precompute()
+    self.P = None
+
   def diagonal(self) -> ArrayLike:
     return self.degree
 
@@ -569,26 +572,39 @@ class UpLaplacian(LinearOperator):
         self.P[cc] = d1, jj, ii
         self.P[cc+1] = d2, ii, jj
         cc += 2
-    self._matvec = self._matvec_precompute
     return self
     #   np.add.at(v, P['vo'], x[P['xi']]*P['weights'])
 
   def _matvec_precompute(self, x: ArrayLike) -> ArrayLike:
+    x = x.reshape(-1)
     self._v.fill(0)
-    self._v += self.degree * x.reshape(-1)
+    self._v += self.degree * x
     np.add.at(self._v, self.P['vo'], x[self.P['xi']]*self.P['weights'])
     return self._v
   
   ## Define the matvec operator using the precomputed degree, the pre-allocate workspace, and the pre-determined sgn pattern
-  def _matvec(self, x: ArrayLike):
+  def _matvec_full(self, x: ArrayLike) -> ArrayLike:
+    x = x.reshape(-1)
     self._v.fill(0)
-    self._v += self.degree * x.reshape(-1)
+    self._v += self.degree * x
     for s_ind, s in enumerate(self.simplices):
       for (f1, f2), sgn_ij in zip(combinations(s.boundary(), 2), self.sgn_pattern):
         ii, jj = self.index(f1), self.index(f2)
         self._v[ii] += sgn_ij * x[jj] * self._wfl[ii] * self._ws[s_ind] * self._wfr[jj]
         self._v[jj] += sgn_ij * x[ii] * self._wfl[jj] * self._ws[s_ind] * self._wfr[ii]
     return self._v
+    
+  def _matvec(self, x: ArrayLike) -> ArrayLike:
+    assert x.ndim == 1 or (x.ndim == 2 and 1 in x.shape)
+    return self._matvec_precompute(x) if self.P is not None else self._matvec_full(x)
+
+  def _matmat(self, X: ArrayLike) -> ArrayLike:
+    n,k = X.shape
+    m = self.shape[0]
+    Y = np.empty(shape=(m,k))
+    for j in range(X.shape[1]):
+      Y[:,j] = self._matvec(X[:,j])
+    return Y
 
 ## From: https://github.com/cvxpy/cvxpy/blob/master/cvxpy/interface/matrix_utilities.py
 def is_symmetric(A) -> bool: