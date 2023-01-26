Last commit for the night, going to run vineyards overnight. Need to fix the displacement problem for vineyards and perhaps simplify the matvecs to streamline the computation

examples/image_complex.py
@@ -18,7 +18,7 @@ def pixel_circle(n):
     return np.exp(e*(abs(x_dist - radius)/width)).reshape((n,n))
   return _circle
 
-n = 5
+n = 9
 C = pixel_circle(n)
 X = C(0)
 assert X.flags.c_contiguous
@@ -54,9 +54,10 @@ I = np.arange(0, D.shape[1])
 R, V = pm.phcol(D, V, I)
 R = R.astype(int).tolil()
 V = V.astype(int).tolil()
-
+R = R.todense()
+V = V.todense()
 OPS_VINES = [vineyards_stats(reset=True)]
-for p in np.linspace(0, 1, num=20):
+for p in np.linspace(0, 1, num=100):
   fv = C(p).flatten()
   update_lower_star(K, R, V, f=lambda s: max(fv[s]), vines=True)
   OPS_VINES.append(vineyards_stats().copy())
@@ -72,8 +73,8 @@ I = np.arange(0, D.shape[1])
 R, V = pm.phcol(D, V, I)
 R = R.astype(int).tolil()
 V = V.astype(int).tolil()
-# R = R.todense()
-# V = V.todense()
+R = R.todense()
+V = V.todense()
 from pbsig.vineyards import move_stats
 OPS_MOVES = [move_stats(reset=True)]
 for p in np.linspace(0, 1, num=20):
@@ -83,8 +84,8 @@ for p in np.linspace(0, 1, num=20):
   OPS_MOVES.append(move_stats().copy())
   print(p)
 
-## Benchmark vineyards
-
+## TODO: solve the bottlenecks to make experimentation feasible!
+import line_profiler
 
 
 