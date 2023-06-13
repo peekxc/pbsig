Bug fixes involving _np in the number of faces of the LO laplacian. Re-creating circle density using new sieve code to try to move towards a robust gradient operation

examples/circle_density.py
@@ -80,12 +80,15 @@ show(row(p, q, r, s))
 
 # %% Parameterized the weight function
 import functools
+from splex.geometry import lower_star_weight
 def codensity(alpha: float):
   """Codensity of a point set _X_ with some bandwidth _alpha_."""
   x_density = gaussian_kde(X.T, bw_method=alpha).evaluate(X.T)
   x_density /= max(x_density)
   x_codensity = max(x_density) - x_density
-  return functools.partial(lambda s, c: max(c[s]), c=x_codensity)
+  return lower_star_weight(x_codensity)
+  # return functools.partial(lambda s, c: max(c[s]), c=x_codensity)
+
 
 
 # %% Change density parameter
@@ -154,23 +157,49 @@ show(p)
 from pbsig.betti import MuFamily
 R = (0.2, 0.4, 0.6, 0.8)
 mu_f = MuFamily(S, codensity_family, p = 1, form='array')
-mu_f.precompute(R, w=1.30, normed=True)
 
-# %% Compare
+# %% 
+mu_f.precompute(R, w=0.0, normed=True, pp=1.0)
+# mu_f()
+
+# [len(mu_f._terms[0][i]) for i in range(4)]
+ 
+# %% Sieve 
+from pbsig.betti import Sieve
+from pbsig.linalg import spectral_rank
+sieve = Sieve(S, codensity_family, p = 1, form='lo')
+sieve.pattern = np.array([R])
+sieve.sift(w=0.0, pp=1.0)
+
+sieve.spectra[0]
+
+np.array(sieve.spectra[2]['eigenvalues'][:25]) - mu_f._terms[0][0]
+
+L = sieve.operator_at(family_index=len(codensity_family)-1, i=0.2, j=0.4)
+sieve.solver(L, pp=1.0)
+
+mu_f._terms[0][99]
+
+sieve_summary = np.ravel(sieve.summarize(f=spectral_rank))
+
+p = figure(width=250, height=250)
+p.line(alpha_family, mu_f(), color='purple')
+p.line(alpha_family, sieve_summary, color='orange')
+show(p)
+
+# %% Check the multiplicity queries match the correct rank 
 p = figure_dgm(x_range=(0, 1), y_range=(0, 1), width=300, height=300)
 p.scatter(family_dgms[:,1], family_dgms[:,2], color=bin_color(family_dgms[:,0]))
 r_width, r_height = R[1]-R[0], R[3]-R[2]
 p.rect(R[0]+r_width/2, R[2]+r_height/2, r_width, r_height, alpha=1.0, fill_alpha=0.0)
 q = figure(width=300, height=300, title="Multiplicity")
 q.step(alpha_family, mu_f(smoothing=None))
-q.step(alpha_family, mu_f(smoothing=True), color='red')
+# q.step(alpha_family, mu_f(smoothing=True), color='red')
 p.toolbar_location = None 
 q.toolbar_location = None
 q.xaxis.axis_label = r"$$\alpha$$"
 show(row(p,q))
 
-
-top10  = lambda x: np.append(np.sort(x)[-10:], [0]*(len(x)-10))
 
 # %% Look at the other relaxations
 from scipy.signal import savgol_filter

src/pbsig/betti.py
@@ -565,11 +565,14 @@ class MuFamily:
     
     ## Construct a single operator or matrix for iteration
     if self.form == "array":
-      D = boundary_matrix(self.S, p=self.p+1) if self.form == "array" else None
+      D = boundary_matrix(self.S, p=self.p+1).astype(np.float64) if self.form == "array" else None
+      solver = eigvalsh_solver(D @ D.T)
     elif self.form == "lo":
       L = up_laplacian(self.S, p=self.p, form="lo")
+      solver = eigvalsh_solver(L) 
     else: 
       raise ValueError("Unknown type") 
+    
 
     ## Traverse the family 
     for i, f in family_it:
@@ -587,7 +590,6 @@ class MuFamily:
             I_norm = pseudo(np.sqrt(I))
             L = diags(I_norm) @ D @ diags(J) @ D.T @ diags(I_norm)
             # L = D @ diags(J) @ D.T
-          solver = eigvalsh_solver(L, **kwargs) ## TODO: change behavior to not be matrix specific! or to transform based on matrix
           self._terms[cc][i] = solver(L, **kwargs)
           # ew_ext = np.sort(np.append(np.repeat(0, len(I)-len(ew)), ew))
           # self._terms[cc][i] = ew_ext * np.sort(I)
@@ -595,13 +597,13 @@ class MuFamily:
         for cc, (I,J) in enumerate([(self._fj, self._fk), (self._fi, self._fk), (self._fj, self._fl), (self._fi, self._fl)]):
           if normed:
             I_sgn = np.sign(abs(I)) # I 
+            # I_sgn = np.where(np.isclose(I, 0), 0.0, 1.0)
             L.set_weights(I_sgn, J, I_sgn) ## TODO: PERHAPS don't set face weights to None, as that defaults to identity. Use entries in {0,1}
             I_norm = pseudo(np.sqrt(L.diagonal())) # degree computation
             L.set_weights(I_norm, J, I_norm)
           else:
             I_norm = pseudo(np.sqrt(I))
             L.set_weights(I_norm, J, I_norm)
-          solver = eigvalsh_solver(L, **kwargs)
           self._terms[cc][i] = solver(L, **kwargs)
       else: 
         raise ValueError("Unknown type")  
@@ -854,7 +856,7 @@ class Sieve:
         for jj,ew in enumerate(ew_split):
           values[ii,jj] = f(ew)
     else:
-      raise NotImplementedError("Haven't implemented yet")
+      raise NotImplementedError("Haven't implemented yet element-wise function yet")
     # or np.allclose(f(np.zeros(10)), np.zeros(10)), 
 
     ## Apply inclusion-exclusion to add the appropriately signed corner points together
@@ -882,6 +884,22 @@ class Sieve:
     else:
       raise NotImplementedError("Array form of projection not implemented")
 
+  def operator_at(self, family_index: int, i: float, j: float, w: float = 0.0, **kwargs):
+    assert family_index < len(self.family) and family_index >= 0, "Invalid family index (out of bounds)"
+    f = self.family[family_index]
+    si, sj = smooth_upstep(lb=i, ub=i+w), smooth_dnstep(lb=j-w, ub=j+self.delta)
+    fp, fq = si(f(self.p_faces)), sj(f(self.q_faces))
+    if self.form == 'lo':
+      I = np.where(np.isclose(fp,0),0.0,1.0)
+      self.laplacian.set_weights(I,fq,I)
+      d = np.sqrt(pseudoinverse(self.laplacian.degrees))
+      if any(np.isnan(d)):
+        nan_ind = np.flatnonzero(np.isnan(d))
+        raise ValueError(f"Failed to project ({i:.5f},{j:.5f}); nan detected in pseudo-inverse of {self.laplacian.degrees[nan_ind[0]]}.")
+      self.laplacian.set_weights(d,fq,d)
+      return self.laplacian
+    else:
+      raise NotImplementedError("Array form of projection not implemented")
   
   def partition_rect():
     """ Increases the discrimatory power by adding two interior points to the pattern"""

src/pbsig/linalg.py
@@ -1000,7 +1000,7 @@ def up_laplacian(S: ComplexLike, p: int = 0, weight: Union[Callable, ArrayLike] 
       # p_faces = list(faces(S, p))        ## need to make a view 
       p_simplices = list(faces(S, p+1))  ## need to make a view 
       _lap_cls = eval(f"UpLaplacian{int(p)}D")
-      lo = _lap_cls(p_simplices, card(S, 0))
+      lo = _lap_cls(p_simplices, card(S, 0), card(S, p))
       if normed:
         if not(symmetric):
           import warnings
@@ -1124,44 +1124,44 @@ class LaplacianOperator:
 
 
 class UpLaplacian0D(laplacian.UpLaplacian0D, UpLaplacianBase):
-  def __init__(self, S: Iterable['SimplexLike'], nv: int):
+  def __init__(self, S: Iterable['SimplexLike'], nv: int, _np: int = 0):
     UpLaplacianBase.__init__(S, nv) ## does error checking on S 
     S = np.fromiter(collapse(S), dtype=np.uint16)
-    laplacian.UpLaplacian0D.__init__(self, S, nv)
+    laplacian.UpLaplacian0D.__init__(self, S, nv, _np)
     self.precompute_degree()
 
 class UpLaplacian0F(laplacian.UpLaplacian0F, UpLaplacianBase):
-  def __init__(self, S: Iterable['SimplexLike'], nv: int):
+  def __init__(self, S: Iterable['SimplexLike'], nv: int, _np: int = 0):
     S = np.fromiter(collapse(S), dtype=np.uint16)
-    laplacian.UpLaplacian0F.__init__(self, S, nv)
+    laplacian.UpLaplacian0F.__init__(self, S, nv, _np)
     self.precompute_degree()
 
 class UpLaplacian1D(laplacian.UpLaplacian1D, UpLaplacianBase):
-  def __init__(self, S: Iterable['SimplexLike'], nv: int):
+  def __init__(self, S: Iterable['SimplexLike'], nv: int, _np: int = 0):
     UpLaplacianBase.__init__(S, nv)
     S = np.fromiter(collapse(S), dtype=np.uint16)
-    laplacian.UpLaplacian1D.__init__(self, S, nv)
+    laplacian.UpLaplacian1D.__init__(self, S, nv, _np)
     self.precompute_degree()
 
-class UpLaplacian1D(laplacian.UpLaplacian1F, UpLaplacianBase):
-  def __init__(self, S: Iterable['SimplexLike'], nv: int):
+class UpLaplacian1F(laplacian.UpLaplacian1F, UpLaplacianBase):
+  def __init__(self, S: Iterable['SimplexLike'], nv: int, _np: int = 0):
     UpLaplacianBase.__init__(S, nv)
     S = np.fromiter(collapse(S), dtype=np.uint16)
-    laplacian.UpLaplacian1F.__init__(self, S, nv)
+    laplacian.UpLaplacian1F.__init__(self, S, nv, _np)
     self.precompute_degree()
     
 class UpLaplacian2D(laplacian.UpLaplacian2D, UpLaplacianBase):
-  def __init__(self, S: Iterable['SimplexLike'], nv: int):
+  def __init__(self, S: Iterable['SimplexLike'], nv: int, _np: int = 0):
     UpLaplacianBase.__init__(S, nv)
     S = np.fromiter(collapse(S), dtype=np.uint16)
-    laplacian.UpLaplacian2D.__init__(self, S, nv)
+    laplacian.UpLaplacian2D.__init__(self, S, nv, _np)
     self.precompute_degree()
     
 class UpLaplacian2F(laplacian.UpLaplacian2F, UpLaplacianBase):
-  def __init__(self, S: Iterable['SimplexLike'], nv: int):
+  def __init__(self, S: Iterable['SimplexLike'], nv: int, _np: int = 0):
     UpLaplacianBase.__init__(S, nv)
     S = np.fromiter(collapse(S), dtype=np.uint16)
-    laplacian.UpLaplacian2F.__init__(self, S, nv)
+    laplacian.UpLaplacian2F.__init__(self, S, nv, _np)
     self.precompute_degree()
 
 ## From: https://github.com/cvxpy/cvxpy/blob/master/cvxpy/interface/matrix_utilities.py

tests/test_eigen.py
@@ -8,23 +8,43 @@ from pbsig.utility import *
 from pbsig.linalg import *
 from splex import simplicial_complex
 from splex import ComplexLike
-
 from itertools import chain
 import networkx as nx
-
 from scipy.linalg import eigh
 from scipy.sparse.linalg import eigsh
 
-G = nx.Graph()
-while (len(list(nx.connected_components(G))) != 1):
-  G = nx.random_geometric_graph(100, radius=0.15, dim=2)
-S = simplicial_complex(chain(G.nodes(), G.edges()))
-fv = np.random.uniform(size=card(S,0), low=0, high=0.01)
+def test_operators():
+  G = nx.Graph()
+  while (len(list(nx.connected_components(G))) != 1):
+    G = nx.random_geometric_graph(100, radius=0.15, dim=2)
+  S = simplicial_complex(chain(G.nodes(), G.edges()), form="tree")
+  S.expand(2)
+  fv = np.random.uniform(size=card(S,0), low=0, high=0.01)
 
-## Normalized Laplacians are quite stable
-LM = up_laplacian(S, p=0, form='array', weight=lower_star_weight(fv), normed=True)
-LO = up_laplacian(S, p=0, form='lo', weight=lower_star_weight(fv), normed=True)
-ew0 = eigsh(LO, k=30)[0]
+  ## Normalized Laplacians are quite stable
+  LM = up_laplacian(S, p=0, form='array', weight=lower_star_weight(fv), normed=True)
+  LO = up_laplacian(S, p=0, form='lo', weight=lower_star_weight(fv), normed=True)
+  ew0_ar = eigsh(LM, k=30)[0]
+  ew0_lo = eigsh(LO, k=30)[0]
+  assert np.allclose(ew0_ar - ew0_lo, 0.0)
+
+  LM = up_laplacian(S, p=1, form='array', weight=lower_star_weight(fv), normed=True)
+  LO = up_laplacian(S, p=1, form='lo', weight=lower_star_weight(fv), normed=True)
+  ew0_ar = eigsh(LM, k=30)[0]
+  ew0_lo = eigsh(LO, k=30)[0]
+  assert np.allclose(ew0_ar - ew0_lo, 0.0)
+
+def test_matvec():
+  for i in range(30):
+    G = nx.Graph()
+    while (len(list(nx.connected_components(G))) != 1):
+      G = nx.random_geometric_graph(100, radius=0.15, dim=2)
+    S = simplicial_complex(chain(G.nodes(), G.edges()))
+    LO = up_laplacian(S, p = 0, form='lo')
+    LM = up_laplacian(S, p = 0, form='array').tocsr()
+    for i in range(1000):
+      x = np.random.uniform(low=0, high=1, size=LO.shape[0])
+      assert max(abs((LM @ x) - (LO @ x))) <= 1e-14
 
 
 fe = np.array([lower_star_weight(fv)(e) for e in faces(S,1)])