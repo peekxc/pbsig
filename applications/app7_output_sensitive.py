More changes to try to identify bug with unbounded calculation. Centralized the weighted laplacian computation

applications/app7_output_sensitive.py
@@ -21,7 +21,7 @@ output_notebook()
 from pbsig.datasets import noisy_circle
 from splex.geometry import delaunay_complex
 np.random.seed(1234)
-X = noisy_circle(10, n_noise=0, perturb=0.05) ## 80, 30, 0.15 
+X = noisy_circle(5, n_noise=0, perturb=0.05) ## 80, 30, 0.15 
 S = sx.rips_complex(X, radius=0.75, p=2)
 
 # %% Peek at the complex 
@@ -37,33 +37,90 @@ dgms = generate_dgm(K, R, simplex_pairs=True)
 ## Plot the persistence diagram in the filter plane
 show(figure_dgm(dgms[1]))
 
-# %% First, re-index to the index filtration
-K.reindex(np.arange(len(K)))
+# %% Capture the creators 
+# 1. First, cone the complex + the filter
+# 2. Second, re-index to the index filtration
+from pbsig.betti import cone_filter
+vid = sx.card(S,0)
+S.insert([sx.Simplex([vid]) + sx.Simplex(s) for s in S])            ## cone the complex
+coned_diam = cone_filter(sx.flag_filter(pdist(X)), vid = vid)       ## generate the coned filter 
+coned_diam_filter = sx.fixed_filter(S, [coned_diam(s) for s in S])  ## optimize it for fast evaluation
+
+## Create a new coned filtration
+K = sx.RankFiltration(S, coned_diam_filter)
+index_filter = sx.fixed_filter(list(sx.faces(K)), np.arange(len(K)))
+K.reindex(index_filter)
+R, V = ph(K, output="RV")
 dgms_index = generate_dgm(K, R, simplex_pairs=True)
 
 ## Plot the persistence diagram in the index-persistence plane
 show(figure_dgm(dgms_index[1]))
 
-# %% 
+
+# %% Produce an array representing the persistence diagram
 from pbsig.betti import betti_query
 from pbsig.linalg import spectral_rank
 K_simplices = list(map(sx.Simplex, sx.faces(K)))
-filter_f = sx.fixed_filter(K_simplices, np.arange(len(K)))
 
-B = lambda i,j: list(betti_query(K_simplices, filter_f, spectral_rank, i = i, j = j, p = 1))[0]
+B = lambda i,j: list(betti_query(K_simplices, index_filter, spectral_rank, i = i, j = j, p = 1))[0]
 M = len(K)
-I0 = B(M // 2, M // 2)
+I0 = B(M // 2, M // 2 + 1)
 I1 = B(0, M // 2)
 I2 = I0 - I1
 
-from pbsig.persistence import persistent_betti, reduction_pHcol, low_entry
 
-a, b = 20.0, 40.0
+
+## The original formulation
 D1 = sx.boundary_matrix(K, p=1).tolil()
 D2 = sx.boundary_matrix(K, p=2).tolil()
-ii = len([s for s in sx.faces(K,1) if s.value <= i]) ## should be 12 
-jj = len([s for s in sx.faces(K,2) if s.value <= j]) ## should be 14
-persistent_betti(D1, D2, ii, jj, summands=False)
+t0 = np.max(D1[:,K.indices(p=1) <= a].shape)
+t1 = np.linalg.matrix_rank(D1[:,K.indices(p=1) <= a].todense())
+
+## This says the 3rd + 4rth is 17! 
+t23 = np.sum([0 <= piv < ii for piv in low_entry(R2[:, K.indices(p=1) <= b])])
+
+## Let's try with the full boundary matrix
+D = sx.boundary_matrix(K).tolil()
+R, V = ph(K, output="RV")
+
+## This says 3rd + 4rth is 12! which seems more correct 
+for cc, (i,s) in enumerate(K):
+  if sx.dim(s) != 2:
+    R[:,cc] = 0
+R.eliminate_zeros()
+np.sum(np.logical_and(np.arange(len(K)) <= b, np.logical_and(0 <= low_entry(R), low_entry(R)  <= a)))
+
+## This says that the fourth term must be all zero 
+D2[:, K.indices(p=2) <= b][K.indices(p=1) >= a,:].todense()
+
+## This says the 3rd + 4rth is 14! 
+list(betti_query(K_simplices, index_filter, spectral_rank, i = M // 2, j = M // 2, p = 1, terms=True))[0]
+
+## Exact 
+np.sum(np.logical_and(dgms_index[1]['birth'] <= a, dgms_index[1]['death'] > b))
+
+from pbsig.persistence import persistent_betti, reduction_pHcol, low_entry
+
+## Back to the basics: says 3rd + 4rth is 14! 
+a, b = M // 2, (M // 2) + 1
+D1 = sx.boundary_matrix(K, p=1).tolil()
+D2 = sx.boundary_matrix(K, p=2).tolil()
+# ii = len([s for s in sx.faces(K,1) if s.value <= a]) ## should be 12 
+# jj = len([s for s in sx.faces(K,2) if s.value <= b]) ## should be 14
+ii = np.sum(K.indices(p=1) <= a)
+jj = np.sum(K.indices(p=2) <= b)
+persistent_betti(D1, D2, ii, jj, summands=True)
+
+sx.faces(K,1)[:ii,:]
+sx.faces(K,2)[:jj,:]
+
+## Check global face poset 
+for i, s in K: assert all([K.index(face) <= i for face in sx.faces(s)])
+
+## Check local face poset wrt (ii,jj)
+S_test = sx.simplicial_complex(sx.faces(K,1)[:ii,:])
+assert np.all([np.all([f in S_test for f in sx.faces(s,1)]) for s in sx.faces(K,2)[:jj,:]])
+
 
 ## Manual calculation using Chen
 R1, R2, V1, V2 = reduction_pHcol(D1, D2)

src/pbsig/betti.py
@@ -246,7 +246,7 @@ def tolerance(m: int, n: int, dtype: type = float):
     return np.max([_machine_eps, spectral_radius * np.max([m,n]) * _min_res])
   return _tol
 
-from pbsig.csgraph import param_laplacian
+from pbsig.csgraph import param_laplacian, WeightedLaplacian
 def betti_query(
   S: Union[LinearOperator, ComplexLike], 
   f: Callable[SimplexConvertible, float],
@@ -263,44 +263,50 @@ def betti_query(
   # L = S if isinstance(S, UpLaplacian) else up_laplacian(S, p=p, form='lo')
   # assert isinstance(L, UpLaplacian)
   # assert is_complex_like(S)
-  B0, B1 = boundary_matrix(S, p = p), boundary_matrix(S, p = p+1)
+  # B0, B1 = boundary_matrix(S, p = p), boundary_matrix(S, p = p+1)
+  # pseudo = lambda x: np.reciprocal(x, where=~np.isclose(x, 0, atol=atol)) # scalar pseudo-inverse 
+  
   yw, fw, sw = f(faces(S, p-1)), f(faces(S, p)), f(faces(S, p+1))
   delta = np.finfo(float).eps 
   atol = kwargs['tol'] if 'tol' in kwargs else 1e-5     
-  pseudo = lambda x: np.reciprocal(x, where=~np.isclose(x, 0, atol=atol)) # scalar pseudo-inverse 
   p_solver = PsdSolver(k = int(card(S, p-1)-1)) if solver is None else solver
   q_solver = PsdSolver(k = int(card(S, p)-1)) if solver is None else solver
   I, J = (np.array([i]), np.array([j])) if isinstance(i, Number) and isinstance(j, Number) else (i,j)
-
   inc_all = smooth_upstep(0, w)
+  
+  ## Get the Weighted Laplacians
+  L_kwargs = dict(normed = True, isometric = False, sign_width = w, form="array") | kwargs
+  Lp = WeightedLaplacian(S, p = p-1, **L_kwargs)
+  Lq = WeightedLaplacian(S, p = p, **L_kwargs)
+
   for ii, jj in zip(I, J):
     assert ii <= jj, f"Invalid point ({ii:.2f}, {jj:.2f}): must be in the upper half-plane"
     fi_inc = smooth_dnstep(lb = ii-w, ub = ii+delta)
     fi_exc = smooth_upstep(lb = ii, ub = ii+w)         
     fj_inc = smooth_dnstep(lb = jj-w, ub = jj+delta)
     
-    ## Get the eigenvalues 
-    # T0 = diags(fi_inc(fw))
-    # T1 = param_laplacian(B0, p_weights=np.ones(len(yw)), q_weights=fi_inc(fw), normed=True, boundary=True)
-    # T2 = param_laplacian(B1, p_weights=np.ones(len(fw)), q_weights=fj_inc(sw), normed=True, boundary=True)
-
-    T1 = param_laplacian(B0, p_weights=inc_all(yw), q_weights=fi_inc(fw), normed=True, boundary=True, **kwargs)
-    T2 = param_laplacian(B1, p_weights=inc_all(fw), q_weights=fj_inc(sw), normed=True, boundary=True, **kwargs)
-    T3 = param_laplacian(B1, p_weights=fi_exc(fw), q_weights=fj_inc(sw), normed=True, boundary=True, **kwargs)
-
+    # T1 = param_laplacian(B0, p_weights=inc_all(yw), q_weights=fi_inc(fw), normed=True, boundary=True, **kwargs)
+    # T2 = param_laplacian(B1, p_weights=inc_all(fw), q_weights=fj_inc(sw), normed=True, boundary=True, **kwargs)
+    # T3 = param_laplacian(B1, p_weights=fi_exc(fw), q_weights=fj_inc(sw), normed=True, boundary=True, **kwargs)
+    
     t0 = matrix_func(fi_inc(fw)) # instead of solver 
-    t1 = matrix_func(p_solver(T1))
-    t2 = matrix_func(q_solver(T2))
-    t3 = matrix_func(q_solver(T3))
+    
+    Lp.reweight(fi_inc(fw), inc_all(yw))
+    t1 = matrix_func(p_solver(Lp.operator()))
+
+    Lq.reweight(fj_inc(sw), inc_all(fw))
+    t2 = matrix_func(q_solver(Lq.operator()))
+    
+    Lq.reweight(fj_inc(sw), fi_exc(fw))
+    t3 = matrix_func(q_solver(Lq.operator()))
+    
     yield (t0, t1, t2, t3) if terms else t0 - t1 - t2 + t3
 
     # ii = 3.49905344
     # ii = 3.50068702
 
 ## Cone the complex
-def cone_filter(x: ArrayLike, vid: int = -1, v_birth: float = -np.inf, collapse_weight: float = np.inf):
-  from scipy.spatial.distance import pdist
-  flag_w = flag_filter(x)
+def cone_filter(f: Callable, vid: int = -1, v_birth: float = -np.inf, collapse_weight: float = np.inf):
   def _cone_weight(s):
     s = Simplex(s)
     if s == Simplex([vid]):
@@ -308,8 +314,15 @@ def cone_filter(x: ArrayLike, vid: int = -1, v_birth: float = -np.inf, collapse_
     elif vid in s:
       return collapse_weight
     else: 
-      return flag_w(s)
+      return f(s)
   return _cone_weight
+
+def cone_complex(S: ComplexLike, vid: int = -1):
+  assert hasattr(S, "add"), f"simplicial complex '{type(S)}' must be mutable"
+  for s in S: 
+    S.add(Simplex([vid]) + Simplex(s))
+  return S
+
 
 ## Changes the eigenvalues! 
 def update_weights(S: ComplexLike, f: Callable[SimplexConvertible, float], p: int, R: ArrayLike, w: float = 0.0, biased: bool = True):

src/pbsig/csgraph.py
@@ -388,23 +388,6 @@ class WeightedLaplacian:
     else:
       raise ValueError(f"Invalid given form '{x}'")
 
-  # def param_laplacian(self, S: sx.ComplexLike, p: int = 0, normed: bool = False, form: str = "array"):
-  #   ## Set the underlying matrix representation
-  #   if form == "array":
-  #     self.p = p
-  #     self.form = form 
-  #     self.normed = normed
-  #     self.bm = sx.boundary_matrix(S, p = p+1)
-  #     self.__dict__.pop('op', None)
-  #   elif form == "lo":
-  #     self.p = p
-  #     self.form = form 
-  #     self.normed = normed
-  #     self.op = up_laplacian(S, p = p, form = "lo")
-  #     self.__dict__.pop('bm', None)
-  #   else:
-  #     raise ValueError(f"Invalid given form '{form}'")
-
   def _matmat(self, X: np.ndarray) -> np.ndarray:
     Y = self.laplacian @ X if self.form == "array" else self.op @ X
     return Y.astype(self.dtype)
@@ -418,6 +401,7 @@ class WeightedLaplacian:
     return y.astype(self.dtype).copy()
 
   def sign(self, x: np.ndarray):
+    """Smoothed version of the non-negative sign function. """
     from pbsig.linalg import smooth_upstep
     return smooth_upstep(lb=0, ub=self.sign_width)(x)
 